297497983f9a4115338b7aabf0709f92c29dced6
Johan Eklund <johan.eklund@finago.com>
2022-09-06 17:27:21 +0300

    Refactor Content and Part components

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,14 +1,25 @@
 const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
-const Content = (props) => {
+
+const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercises}
+      {props.part}: {props.exercises}
     </p>
   );
 };
-const Total = (props) => {};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part} exercises={props.exercises} />
+    </div>
+  );
+};
+const Total = (props) => {
+  return <p>Number of exercises: {props.exercises}</p>;
+};
 
 const App = () => {
   const course = "Half stack application development";
@@ -25,8 +36,7 @@ const App = () => {
       <Content part={part1} exercises={exercises1} />
       <Content part={part2} exercises={exercises2} />
       <Content part={part3} exercises={exercises3} />
-      <Total />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
